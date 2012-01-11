0641053efcc183ce4910ab976e2815fe18076b15
joaonc <joaonc@gmail.com>
2012-01-11 21:15:51 +0000

    Logging enabled and a few other things.

--- src/main/java/com/simplecash/ui/desktop/main/SimpleCashDialog.java
+++ src/main/java/com/simplecash/ui/desktop/main/SimpleCashDialog.java
@@ -1,7 +1,10 @@
 package com.simplecash.ui.desktop.main;
 
+import com.simplecash.dal.declaration.IDatabaseManager;
+import com.simplecash.dal.implementation.DatabaseManagerDAO;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.slf4j.*;
 
 import javax.swing.*;
 import java.awt.event.*;
@@ -10,6 +13,8 @@ import java.util.ResourceBundle;
 
 public class SimpleCashDialog extends JDialog implements ActionListener {
     private JPanel contentPane;
+
+    final Logger logger = LoggerFactory.getLogger(SimpleCashDialog.class);
     private final ResourceBundle simpleCashDialogResourceBundle =
             PropertyResourceBundle.getBundle(SimpleCashDialog.class.getCanonicalName());
 
@@ -27,17 +32,16 @@ public class SimpleCashDialog extends JDialog implements ActionListener {
      * Create the menu for the dialog.
      */
     private void createMenu() {
-        JMenuBar menuBar = new JMenuBar();
-
         // http://docs.oracle.com/javase/tutorial/uiswing/components/menu.html
-        // http://www.exampledepot.com/egs/javax.swing/menu.html
-        // http://www.jetbrains.com/idea/features/i18n_support.html
+
+        JMenuBar menuBar = new JMenuBar();
 
         // File menu
         JMenu menu_file = new JMenu(simpleCashDialogResourceBundle.getString("menu_file"));
         menu_file.setMnemonic(KeyEvent.VK_F);
         JMenuItem menu_file_login = new JMenuItem(simpleCashDialogResourceBundle.getString("menu_file_login"));
         menu_file_login.setActionCommand(SimpleCashDialogAction.menu_file_login_logout);
+        menu_file_login.addActionListener(this);
         menu_file.add(menu_file_login);
         menu_file.addSeparator();
         JMenuItem menu_file_exit = new JMenuItem(simpleCashDialogResourceBundle.getString("menu_file_exit"));
@@ -50,7 +54,16 @@ public class SimpleCashDialog extends JDialog implements ActionListener {
         JMenu menu_admin = new JMenu(simpleCashDialogResourceBundle.getString("menu_admin"));
         JMenuItem menu_admin_createdb = new JMenuItem(simpleCashDialogResourceBundle.getString("menu_admin_createdb"));
         menu_admin_createdb.setActionCommand(SimpleCashDialogAction.menu_admin_createdb);
+        menu_admin_createdb.addActionListener(this);
         menu_admin.add(menu_admin_createdb);
+        JMenuItem menu_admin_updatedb = new JMenuItem(simpleCashDialogResourceBundle.getString("menu_admin_updatedb"));
+        menu_admin_updatedb.setActionCommand(SimpleCashDialogAction.menu_admin_updatedb);
+        menu_admin_updatedb.addActionListener(this);
+        menu_admin.add(menu_admin_updatedb);
+        JMenuItem menu_admin_populatedb = new JMenuItem(simpleCashDialogResourceBundle.getString("menu_admin_populatedb"));
+        menu_admin_populatedb.setActionCommand(SimpleCashDialogAction.menu_admin_populatedb);
+        menu_admin_populatedb.addActionListener(this);
+        menu_admin.add(menu_admin_populatedb);
         menuBar.add(menu_admin);
 
         // Help menu
@@ -69,7 +82,14 @@ public class SimpleCashDialog extends JDialog implements ActionListener {
         } else if (SimpleCashDialogAction.menu_file_login_logout.equals(event.getActionCommand())) {
             // TODO: log in/out
         } else if (SimpleCashDialogAction.menu_admin_createdb.equals(event.getActionCommand())) {
-
+            IDatabaseManager dbMgr = new DatabaseManagerDAO();
+            dbMgr.createDatabaseSchema();
+        } else if (SimpleCashDialogAction.menu_admin_updatedb.equals(event.getActionCommand())) {
+            IDatabaseManager dbMgr = new DatabaseManagerDAO();
+            dbMgr.updateDatabaseSchema();
+        } else if (SimpleCashDialogAction.menu_admin_populatedb.equals(event.getActionCommand())) {
+            IDatabaseManager dbMgr = new DatabaseManagerDAO();
+            dbMgr.populateWithTestData();
         }
     }
 }
