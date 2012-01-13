677f59179c25140fee916cc401bbe4bec3ec9b0e
joaonc <joaonc@gmail.com>
2012-01-13 20:41:23 +0000

    Got transactions to work, but only if begin/commit (or rollback) are declared explicitly. Not optimal. Going to check with the community if someone can help with this.

--- src/main/java/com/simplecash/dal/DatabaseManagerDAO.java
+++ src/main/java/com/simplecash/dal/DatabaseManagerDAO.java
@@ -5,6 +5,7 @@ import com.simplecash.object.*;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 
@@ -14,8 +15,7 @@ import java.util.Date;
 public class DatabaseManagerDAO {
 
     public void createDatabaseSchema() {
-        // To not use the Configuration object below
-        // Using Hibernate's SchemaExport Feature from within a Spring/JPA Context
+        // To not use the Configuration object below for SchemaExport, check out
         // http://hillert.blogspot.com/2010/05/using-hibernates-schemaexport-feature.html
 
         Configuration configuration = new Configuration();
@@ -35,8 +35,10 @@ public class DatabaseManagerDAO {
         bank.setName("ContentName");
         bank.setCode("ContentCode");
 
+        RepositoryFactory.getEntityManager().getTransaction().begin();
         BankRepository bankRepository = RepositoryFactory.getRepository(BankRepository.class);
-        bankRepository.saveAndFlush(bank);
+        bankRepository.save(bank);
+        bankRepository.flush();
+        RepositoryFactory.getEntityManager().getTransaction().commit();
     }
-
 }
